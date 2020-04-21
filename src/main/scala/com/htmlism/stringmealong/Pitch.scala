5b5ea614713fe5df427ffc07f6caf56cebdbab76
Mark Canlas <github@htmlism.com>
2020-04-20 21:54:25 -0400

    thirds, fifths

--- src/main/scala/com/htmlism/stringmealong/Pitch.scala
+++ src/main/scala/com/htmlism/stringmealong/Pitch.scala
@@ -1,9 +1,30 @@
 package com.htmlism.stringmealong
 
+object Pitch {
+  @scala.annotation.tailrec
+  def semitones(pitch: Pitch, n: Int): Pitch =
+    if (n < 1)
+      pitch
+    else
+      semitones(pitch.s, n - 1)
+}
+
 case class Pitch(note: Note, octave: Octave) {
-  def sharp: Pitch =
+  def s: Pitch =
     if (note.n < 12)
       this.copy(note = Note(note.n + 1))
     else
       Pitch(Note(1), Octave(this.octave.n + 1))
+
+  def b: Pitch =
+    if (note.n == 1)
+      Pitch(Note(12), Octave(this.octave.n - 1))
+    else
+      this.copy(note = Note(note.n - 1))
+
+  def third: Pitch =
+    Pitch.semitones(this, 4)
+
+  def fifth: Pitch =
+    Pitch.semitones(this, 7)
 }
--- src/test/scala/com/htmlism/stringmealong/PitchSpec.scala
+++ src/test/scala/com/htmlism/stringmealong/PitchSpec.scala
@@ -5,10 +5,18 @@ import org.scalatest.matchers.should.Matchers
 
 class PitchSpec extends AnyFunSuite with Matchers {
   test("the next pitch after C2 is D2") {
-    C.o2.sharp shouldBe Cs.o2
+    C.o2.s shouldBe Cs.o2
   }
 
   test("the next pitch after B2 is C3") {
-    B.o2.sharp shouldBe C.o3
+    B.o2.s shouldBe C.o3
+  }
+
+  test("a third from C2 is E2") {
+    C.o2.third shouldBe E.o2
+  }
+
+  test("a fifth from C2 is G2") {
+    C.o2.fifth shouldBe G.o2
   }
 }
