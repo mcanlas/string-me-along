11b67735d39a84ceab46519318e6f42324b3c507
Mark Canlas <github@htmlism.com>
2020-11-21 14:19:07 -0500

    add interval stacking

---
+++ src/test/scala/com/htmlism/stringmealong/IntervalStackingSpec.scala
@@ -0,0 +1,22 @@
+package com.htmlism.stringmealong
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class IntervalStackingSpec extends AnyFunSuite with Matchers {
+  test("major chord: major minor stacking") {
+    C.raisedBy(Interval.MajorThird) shouldBe E
+
+    C
+      .raisedBy(Interval.MajorThird)
+      .raisedBy(Interval.MinorThird) shouldBe G
+  }
+
+  test("minor chord: minor major stacking") {
+    C.raisedBy(Interval.MinorThird) shouldBe Eb
+
+    C
+      .raisedBy(Interval.MinorThird)
+      .raisedBy(Interval.MajorThird) shouldBe G
+  }
+}
