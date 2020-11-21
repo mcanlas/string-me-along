3c07a1a7e3e201c52b3b0ac232ac9ec2d9695f91
Mark Canlas <github@htmlism.com>
2020-11-21 18:06:45 -0500

    add accidentals

---
+++ src/test/scala/com/htmlism/stringmealong/AccidentalsSpec.scala
@@ -0,0 +1,14 @@
+package com.htmlism.stringmealong
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class AccidentalsSpec extends AnyFunSuite with Matchers {
+  test("sharps") {
+    List.iterate(F, 7)(_.raisedBy(Interval.PerfectFifth)) shouldBe List(F, C, G, D, A, E, B)
+  }
+
+  test("flats") {
+    List.iterate(B, 7)(_.raisedBy(Interval.PerfectFourth)) shouldBe List(B, E, A, D, G, C, F)
+  }
+}
