12cc47f83060085be3d08114d4a102da3699040a
Mark Canlas <github@htmlism.com>
2020-11-22 04:16:08 -0500

    add major scale degrees

---
+++ src/main/scala/com/htmlism/stringmealong/MajorScaleDegree.scala
@@ -0,0 +1,32 @@
+package com.htmlism.stringmealong
+
+case class MajorScaleDegree(n: Int, semitones: Int) {
+  def spell: String = {
+    val accidental =
+      semitones match {
+        case 1 => "#"
+        case 2 => "x"
+        case 0 => ""
+        case -1 => "b"
+        case -2 => "bb"
+        case _ =>
+          "unknown notation"
+      }
+
+    n.toString + accidental
+  }
+}
+
+object MajorScaleDegree {
+  implicit val majorScaleDegreeOrdinal: MusicallyOrdinal[MajorScaleDegree] =
+    new MusicallyOrdinal[MajorScaleDegree] {
+      def sharpen(x: MajorScaleDegree): MajorScaleDegree =
+        x.copy(semitones = x.semitones + 1)
+
+      def flatten(x: MajorScaleDegree): MajorScaleDegree =
+        x.copy(semitones = x.semitones - 1)
+    }
+
+  val Root: MajorScaleDegree =
+    MajorScaleDegree(1, 0)
+}
---
+++ src/test/scala/com/htmlism/stringmealong/MajorScaleDegreeSpec.scala
@@ -0,0 +1,40 @@
+package com.htmlism.stringmealong
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+import com.htmlism.stringmealong.syntax._
+
+class MajorScaleDegreeSpec extends AnyFunSuite with Matchers {
+  test("modes as major scale modifications") {
+    for (s <- Scale.Diatonic.all) {
+      val majorScaleIntervalsWithRoot =
+        Interval.Root :: Scale.MajorScale.intervals
+
+      val diatonicScaleIntervalsWithRoot =
+        Interval.Root :: s.intervals
+
+      val modifiedMajorScale =
+        for (n <- 1 to 8) yield {
+          val majorSemitones =
+            majorScaleIntervalsWithRoot
+              .take(n)
+              .foldLeft(Note(0))((n, i) => n.raisedBy(i))
+
+          val diatonicSemitones =
+            diatonicScaleIntervalsWithRoot
+              .take(n)
+              .foldLeft(Note(0))((n, i) => n.raisedBy(i))
+
+          val semitonesDelta =
+            diatonicSemitones.n - majorSemitones.n
+
+          MajorScaleDegree(n, semitonesDelta)
+        }
+
+      println {
+        Scale.Diatonic.label(s) + "\n  " + modifiedMajorScale.map(_.spell).mkString(", ")
+      }
+    }
+  }
+}
