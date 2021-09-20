ab4a9dc2a95a0e709510a68382b930dbaf82d0b6
Mark Canlas <github@htmlism.com>
2021-09-20 12:26:34 -0400

    scalatest 3.2.10

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -30,7 +30,7 @@ object ProjectPlugin extends AutoPlugin {
         p.settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.2.9")
 
       def withTesting: Project =
-        p.settings(libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.9" % "test")
+        p.settings(libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.10" % "test")
     }
   }
 }
