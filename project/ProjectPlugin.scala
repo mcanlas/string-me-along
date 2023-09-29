c9461140090fe04297717d50649a6a4b213dbf71
Mark Canlas <github@htmlism.com>
2023-09-28 21:35:49 -0400

    cats-effect 3.5.2 :smile_cat:

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -23,7 +23,7 @@ object ProjectPlugin extends AutoPlugin {
         p.settings(libraryDependencies += "org.typelevel" %% "cats-core" % "2.9.0")
 
       def withEffectMonad: Project =
-        p.settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.5.1")
+        p.settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.5.2")
 
       def withTesting: Project =
         p.settings(libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.16" % "test")
