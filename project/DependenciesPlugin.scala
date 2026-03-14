ad1cc9c029ca6f01d56656a81c7560df48a05a9b
Mark Canlas <github@htmlism.com>
2026-03-13 22:01:28 -0400

    cats-effect 3.7.0 :bubbles:

--- project/DependenciesPlugin.scala
+++ project/DependenciesPlugin.scala
@@ -10,7 +10,7 @@ object DependenciesPlugin extends AutoPlugin {
         p.settings(libraryDependencies += "org.typelevel" %% "cats-core" % "2.13.0")
 
       def withEffectMonad: Project =
-        p.settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.6.3")
+        p.settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.7.0")
 
       def withTesting: Project =
         p.settings(libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.19" % "test")
